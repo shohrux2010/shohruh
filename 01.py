8f7ace7e5fffa791063ecf4e3fee17121deb0830
shohrux2010 <xakimboyevshohruh@gmail.com>
2025-02-05 15:18:38 +0500

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,8 @@
+toys = ('bus','car','bear','dino','snake','lizard') # o'zgarmas ro'yxat
+toys = list(toys) # o'zgarmas ro'yxatni oddiy ro'yxatga (List) aylantiramiz
+# Ro'yxatga o'zgartirishlar kiritamiz
+toys.append('dragon')
+toys.remove('bus')
+toys[1] = 'mcqueen'
+toys = tuple(toys) # Ro'yxatni qaytadan o'zgarmas ro'yxatga (Tuple) aylantiramiz
+print(*toys)
